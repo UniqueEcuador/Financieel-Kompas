d5d8be78a4bedb141ab107bdf9554aeb49f4dde2
UniqueEcuador <uniqueecuador@proton.me>
2026-05-09 12:27:35 +0200

    Add files via upload

---
+++ Qwen_javascript_20260509_jr7xf1uz9.js
@@ -0,0 +1,8 @@
+// Voorbeeld: ritme-vragen als optionele velden in de state
+state.ritme = {
+  payday: '', // wanneer komt salaris?
+  adminMoment: '', // wanneer is er tijd voor administratie?
+  contactPreference: '', // app/mail/bel
+  energyHigh: '', // wanneer zijn jullie het scherpst?
+  energyLow: '' // wanneer is het druk/chaotisch?
+};
